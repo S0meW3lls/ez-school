f037b5fa0d7371846df40f323c042c6247bca9a5
samuel <samueldeguio@gmail.com>
2026-01-18 19:22:53 +0100

    Initial Commit

---
+++ src/main/java/com/samueldeguio/ezschool/EzschoolApplication.java
@@ -0,0 +1,13 @@
+package com.samueldeguio.ezschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzschoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samueldeguio/ezschool/EzschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueldeguio.ezschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
