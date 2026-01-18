fbe3122fccae813a82698bc0db1dee95759f9b8d
samuel <samueldeguio@gmail.com>
2026-01-18 20:38:49 +0100

    feat: add lombok annotations

--- src/main/java/com/samueldeguio/ezschool/models/ContactRequest.java
+++ src/main/java/com/samueldeguio/ezschool/models/ContactRequest.java
@@ -1,31 +1,13 @@
 package com.samueldeguio.ezschool.models;
 
+import lombok.Data;
+
+@Data
 public class ContactRequest {
+
 
     private String name;
     private String email;
     private String subject;
     private String message;
-
-    public String getName() { return this.name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return this.email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getSubject() { return this.subject; }
-    public void setSubject(String subject) { this.subject = subject; }
-
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
-
-    @Override
-    public String toString() {
-        return "ContactRequest{" +
-                "name='" + this.name + '\'' +
-                ", email='" + this.email + '\'' +
-                ", subject='" + this.subject + '\'' +
-                ", message='" + this.message + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/samueldeguio/ezschool/services/ContactRequestService.java
+++ src/main/java/com/samueldeguio/ezschool/services/ContactRequestService.java
@@ -1,14 +1,14 @@
 package com.samueldeguio.ezschool.services;
 
 import com.samueldeguio.ezschool.models.ContactRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ContactRequestService {
-
-    private static final Logger logger = LoggerFactory.getLogger(ContactRequestService.class);
 
     /**
      * Saves the request to the database
@@ -18,8 +18,7 @@ public class ContactRequestService {
      */
     public boolean save(ContactRequest request) {
         // TODO: actually store the request, for now just log it
-        logger.info(request.toString());
-
+        log.info(request.toString());
         return true;
     }
 }
